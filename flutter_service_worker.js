2f38c6c92c20814e071704d3ab237011926f806e
Sérgio Deivis <gtisergio@gmail.com>
2026-01-29 14:18:06 -0300

    1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "628b7990bec6811c40b0d05464192360",
-"assets/AssetManifest.bin.json": "3d08ae05386d99ffd2fcb1f5f29c3cc8",
-"assets/AssetManifest.json": "febb7219421548bbc71c008b84d8f8af",
+const RESOURCES = {"assets/AssetManifest.bin": "ad480d9a7c363c071514cbb3e1d021b5",
+"assets/AssetManifest.bin.json": "4137f32028066359fb4e667ad8c0d238",
+"assets/AssetManifest.json": "8d6b04eccc79bde118b34af26ab7d3ea",
 "assets/assets/images/n1.jpeg": "d6efc1206b1c4acb6fec07bf161ec716",
 "assets/assets/images/n2.jpeg": "13982a461a5954585c6d02a48d9a5613",
 "assets/assets/images/n3.jpeg": "152fe2e9bbed93cec77867bfbb9433ba",
@@ -23,6 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "628b7990bec6811c40b0d05464192360
 "assets/assets/images/naodilogo8.png": "0a9f287839dd50d8f03524a7b396384a",
 "assets/assets/images/naodilogo9.png": "64f86e44dc2ab452d9362a552d83bb79",
 "assets/assets/images/naodilogodesenho.svg": "5d2b1ca6d62cbf76e919e5e868ac86e1",
+"assets/assets/images/whatsappsvg.svg": "31f1fa77dd03688919625a8a6bbc7223",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "cc70e1c498899e2104c80584b449e009",
 "assets/NOTICES": "00e8a0845ecba905d819791e9cbfe718",
@@ -42,16 +43,16 @@ const RESOURCES = {"assets/AssetManifest.bin": "628b7990bec6811c40b0d05464192360
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "d19effc7a1b1109d4f1d19e3642aefe3",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d24fa1bf3ca3d41f1dbaf4225ebf9a92",
+"flutter_bootstrap.js": "adcbc828b32e258f32ba01f47f9594fc",
 "icons/Icon-192.png": "095f15ac0f8a42128c3c148206530bcf",
 "icons/Icon-512.png": "6e450bf7070eb8bb1f7730eceb88bb69",
 "icons/Icon-maskable-192.png": "0625e45bb0b9d480283f4cc2a3c7fd90",
 "icons/Icon-maskable-512.png": "e91ace6b40a73331cb41997cea217451",
-"index.html": "f8c5b7eba515ca37843eb55ba57fcb7c",
-"/": "f8c5b7eba515ca37843eb55ba57fcb7c",
-"index.html.bak": "a019898355cadad7b393a444dd1658a3",
-"main.dart.js": "8cbca4eb851ffde8a217d45a5a69ea11",
-"manifest.json": "5bbb6f8fc24dafc6bae387073e0bb602",
+"index.html": "61d425c69a2b8b1fbfb1b228ea7f0924",
+"/": "61d425c69a2b8b1fbfb1b228ea7f0924",
+"index.html.bak": "f8c5b7eba515ca37843eb55ba57fcb7c",
+"main.dart.js": "a17f08c3e59b84a31c396cab405bb75c",
+"manifest.json": "278135996e588bc0596ee7db45ada062",
 "version.json": "4c6bf301f88add8aff3cdb3782cffa41"};
 // The application shell files that are downloaded before a service worker can
 // start.
